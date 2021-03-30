2985449d4e1871fadede25295731956a375c2170
Arianna Carotti <arianna.carotti@gmail.com>
2021-03-30 16:51:10 +0200

    stampato prezzo finale

--- js/script.js
+++ js/script.js
@@ -9,22 +9,24 @@ calculateButton.addEventListener('click', function() {
     
     var addCheckBox = document.getElementsByClassName('addon');
     console.log(addCheckBox);
-
+// esiste già un prezzo di base che dovrà essere implementato in base al valore delle checkbox checked;
     var prezzoBaseBurger = 15 ;
-
+// estrarre il valore delle checkbox (stringa) con un ciclo FOR ;
     for ( var i=0; i < addCheckBox.length; i++ ) {
 
         var thisCheck = addCheckBox[i];
+        // convertire in numero (parseInt) per utilizzarlo nel calcolo del prezzo finale;
         var prezzoAsNumber = parseInt(thisCheck.value);               
 
         if ( thisCheck.checked == true ) {
 
-            prezzoBaseBurger += prezzoAsNumber;
-            console.log(prezzoBaseBurger)
-
-        }
+            prezzoBaseBurger += prezzoAsNumber;            
+        }        
     }
-// estrarre il valore delle checkbox (stringa) con un ciclo FOR ;
-// convertire in numero (parseInt) per utilizzarlo nel calcolo del prezzo finale;
-// esiste già un prezzo di base che dovrà essere implementato in base al valore delle checkbox checked;
+
+    var prezzoFinale = prezzoBaseBurger.toFixed(2);
+
+
+// STAMPO
+document.getElementById('final-price').innerHTML ='$ ' + prezzoFinale;
 });
